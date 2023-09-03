a5f60860bbeb8ee0af7099d1585ced6cbf2294dd
Johnson noah <143536219+arishacke@users.noreply.github.com>
2023-09-03 00:06:24 -0700

    Update index.php

--- dist/index.php
+++ dist/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="UTF-8">
-  <title>WELCOM</title>
+  <title>WELCOME</title>
   <link rel="stylesheet" href="./style.css">
 
 </head>
